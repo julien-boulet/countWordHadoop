f2b5000a72fdb3a55cc6ed0e0195ed35c9ef3db2
julien boulet <julien.boulet@cacd2.fr>
2019-10-06 00:36:37 +0200

    bugfix

--- script/script.sh
+++ script/script.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+# to remove :WARNING: HADOOP_PREFIX has been replaced by HADOOP_HOME. Using value of HADOOP_PREFIX.
+export HADOOP_HOME=$HADOOP_PREFIX
+export PATH=$PATH:$HADOOP_HOME/bin
+export PATH=$PATH:$HADOOP_HOME/sbin
+export HADOOP_MAPRED_HOME=${HADOOP_HOME}
+export HADOOP_COMMON_HOME=${HADOOP_HOME}
+export HADOOP_HDFS_HOME=${HADOOP_HOME}
+export YARN_HOME=${HADOOP_HOME}
+unset HADOOP_PREFIX
+
 # create the input directory on HDFS
 hadoop fs -mkdir -p input
 # put the input files to all the datanodes on HDFS
